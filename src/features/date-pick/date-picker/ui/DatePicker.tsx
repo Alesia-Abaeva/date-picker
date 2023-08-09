a872609ec2b90a43c058ececc0be8728d014e0d8
Alesia <alesia.litvinenko@gmail.com>
2023-08-09 13:31:06 +0300

    feat: add show calendar on click on input

--- src/features/date-pick/date-picker/ui/DatePicker.tsx
+++ src/features/date-pick/date-picker/ui/DatePicker.tsx
@@ -5,15 +5,54 @@ import "./DatePicker.css";
 const DatePicker: React.FC<DatePickerProps> = ({ value, onChange }) => {
   const [showPopup, setShowPopup] = React.useState(false);
 
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    const element = ref.current;
+
+    if (!element) return;
+
+    const onDocumentClick = (e: MouseEvent) => {
+      const target = e.target;
+
+      /**
+       * Checking for typing.
+       * we can click on the elements, on the nodes
+       * and therefore check if you can specify specifics,
+       * like inputNode or svg or else
+       */
+      if (!(target instanceof Node)) {
+        return;
+      }
+
+      // check if there is content, if there is, we do nothing
+      if (element.contains(target)) {
+        return;
+      }
+
+      setShowPopup(false);
+    };
+
+    document.addEventListener("click", onDocumentClick);
+
+    return () => {
+      document.removeEventListener("click", onDocumentClick);
+    };
+  }, []);
+
   const onFocus = () => {
     setShowPopup(true);
   };
 
   return (
     <>
-      <div>
+      <div className="CalendarContainer" ref={ref}>
         <input type="text" onFocus={onFocus} />
-        {showPopup && <CalendarPopup value={value} onChange={onChange} />}
+        {showPopup && (
+          <div className="CalendarWrapper">
+            <CalendarPopup value={value} onChange={onChange} />
+          </div>
+        )}
       </div>
     </>
   );
