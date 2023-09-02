36db57ca497eaba4185227acb64c9d06e304d25e
Alesia <alesia.litvinenko@gmail.com>
2023-09-02 14:33:20 +0300

    feat: add test on selected date

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -6,6 +6,7 @@ import { Features } from "features/date-pick";
 const min = new Date(2023, 7, 28);
 const max = new Date(2023, 7, 29);
 const initialDate = new Date(2024, 7, 28);
+const todayDate = new Date();
 const tomorrow = new Date();
 tomorrow.setDate(tomorrow.getDate() + 1);
 
@@ -64,26 +65,68 @@ describe("Date Picker test", () => {
     await userEvent.click(screen.getByTestId("date-picker-input"));
     expect(screen.queryByTestId("dp-popup")).toBeInTheDocument();
 
-    const today = screen
+    const todayCells = screen
       .getAllByTestId("date-picker-popup-cell-date")
       .filter((item) => item.classList.contains("CurrentDate--today"));
 
-    console.log(today);
+    expect(todayCells).toHaveLength(1);
 
-    expect(today[0]).toHaveClass("CurrentDate--today");
-    expect(today).toHaveLength(1);
-    // expect(today).toBeInTheDocument();
-
-    // date-picker-popup-cell
+    const today = todayCells[0];
+    expect(today).toHaveClass("CurrentDate--today");
+    expect(today).toHaveTextContent(todayDate.getDate().toString());
   });
 
-  // it("should select date");
+  it("should highlight selected date", async () => {
+    render(<Features.DatePicker value={initialDate} onChange={() => {}} />);
+
+    // open popup
+    await userEvent.click(screen.getByTestId("date-picker-input"));
+    expect(screen.queryByTestId("dp-popup")).toBeInTheDocument();
+
+    const selectedCell = screen
+      .getAllByTestId("date-picker-popup-cell-date")
+      .filter((item) => item.classList.contains("CurrentDate--selected"));
+
+    expect(selectedCell).toHaveLength(1);
+
+    const selected = selectedCell[0];
+    expect(selected).toHaveClass("CurrentDate--selected");
+    expect(selected).toHaveTextContent(initialDate.getDate().toString());
+  });
+
+  it("should select date", async () => {
+    const onChange = jest.fn();
+
+    render(
+      <Features.DatePicker
+        value={initialDate}
+        min={min}
+        max={max}
+        onChange={onChange}
+      />
+    );
+
+    const input = screen.getByTestId("date-picker-input");
+    await userEvent.click(input); // open popup
+
+    const selectCells = screen
+      .getAllByTestId("date-picker-popup-cell-date")
+      .filter((item) => item.textContent === "15"); // get 15-th date
+
+    expect(selectCells).toHaveLength(1);
+
+    await userEvent.click(selectCells[0]);
+
+    // popup must close
+    expect(screen.queryByTestId("dp-popup")).not.toBeInTheDocument();
+    expect(onChange).toBeCalledTimes(1);
+    expect(onChange).toBeCalledWith(new Date(2024, 7, 15));
+    expect(input).toHaveValue("15-08-2022");
+  });
 
   // it("should apply valid date from input on outside click");
 
   // it("should reset invalid date from input on outside click");
-
-  // it("should highlight selected date");
 
   // it("should show correct month in popup");
   // it("should move to the prev month");
